fc62c06b1594fa1ad7fe25b3833cd2b98fc2fec4
Atik masud <Atik03072007@gmail.com>
2026-09-16 21:57:53 +0600

    Add files via upload

---
+++ assets/js/smoother-script.js
@@ -0,0 +1,18 @@
+/*
+ * Custom Script
+ * Developed & Customized by Atik Masud
+ */
+
+$(function () {
+
+  gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+  ScrollTrigger.normalizeScroll(false);
+
+  // create the smooth scroller FIRST!
+  let smoother = ScrollSmoother.create({
+    smooth: 2,
+    effects: true,
+  });
+
+});
